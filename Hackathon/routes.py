e384c4f1252253c2c7ae2d3baca7642226356d04
Gaurang Agrawal <gaurang3000@gmail.com>
2021-03-06 09:51:50 +0530

    error fix

--- Hackathon/routes.py
+++ Hackathon/routes.py
@@ -15,6 +15,10 @@ def logout():
 	logout_user()
 	return redirect(url_for('login'))
 
+@app.route("/booking")
+def booking():
+	return render_template('booking.html')
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
@@ -32,14 +36,14 @@ def login():
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	if current_user.is_authenticated:
-		return redirect(url_for('home'))
+		return render_template('index.html')
 	form = RegisterationForm()
-	print("hello")
+	form2 = LoginForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 		user = User(email=form.email.data, password=hashed_password, company=form.company.data)
 		db.session.add(user)
 		db.session.commit()
 		flash('Your Account Has Been Successfully Created. Now you can Log In', 'success')
-		return redirect(url_for('login'))
+		return render_template('login.html', form=form2)
 	return render_template('signup.html', form=form)
